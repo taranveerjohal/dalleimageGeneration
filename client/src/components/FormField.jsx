e743a4b2889182c50a7b2dd27ce8d2b45fc1b9cc
taranveerjohal <32153442+taranveerjohal@users.noreply.github.com>
2023-02-01 02:29:55 +1030

    DOne

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -12,7 +12,7 @@ const FormField = ({ label, type, placeholder, name, value, handleChange, isSurp
            </label>
               {isSurpsriseMe && (
                 <button 
-                    className='bg-gray-100 font-semibold text-xs text-gray-500 text-[14px] px-2 py-1 rounded-md leading-tight
+                    className='bg-gray-100 font-semibold text-xs text-gray-500 text-[14px] px-2 py-1 rounded-md 
                     shadow-md hover:bg-gray-300 hover:shadow-lg hover:scale-110 focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out'
                     onClick={isSurpsriseMe}
                     type='button'
@@ -21,17 +21,29 @@ const FormField = ({ label, type, placeholder, name, value, handleChange, isSurp
                 </button>
                 )}
         </div>
+       {label === 'Prompt' ? 
+        <textarea 
+        type={type}
+        placeholder={placeholder}
+        id={name}
+        name={name}
+        value={value}
+        onChange={handleChange}
+        rows='4'
+        required
+        className='block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 focus:ring-[#6469ff] focus:border-[#6469ff] outline-none'
+    />
+     :
         <input 
-            type={type}
-            placeholder={placeholder}
-            id={name}
-            name={name}
-            value={value}
-            onChange={handleChange}
-            required
-            className=' border w-full border-gray-300 bg-gray-50 rounded-md text-sm text-gray-900 p-3 focus:ring-[#6469ff] focus:border-[#6469ff] outline-none'
-        />
-     
+        type={type}
+        placeholder={placeholder}
+        id={name}
+        name={name}
+        value={value}
+        onChange={handleChange}
+        required
+        className=' border w-full border-gray-300 bg-gray-50 rounded-md text-sm text-gray-900 p-3 focus:ring-[#6469ff] focus:border-[#6469ff] outline-none'
+    /> }
     </div>
   )
 }
